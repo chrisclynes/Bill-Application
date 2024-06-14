9eaaf2d828d8660af665b82b92ee8865369e7939
chrisclynes <chrisclynes@live.com>
2024-06-13 23:05:07 -0500

    Adding modal for edit rows

--- src/app/components/edit-row/edit-row.component.ts
+++ src/app/components/edit-row/edit-row.component.ts
@@ -1,12 +1,15 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { Component, Input, Output, EventEmitter, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Bill } from '../../models/bill/bill.model';
 import { DatePipe, CommonModule } from '@angular/common';
+import { MAT_DIALOG_DATA, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-edit-row',
   standalone: true,
-  imports: [FormsModule, CommonModule], // Include FormsModule here
+  imports: [FormsModule, CommonModule, MatDialogModule, MatButtonModule, MatInputModule], // Include FormsModule and Angular Material modules here
   templateUrl: './edit-row.component.html',
   styleUrls: ['./edit-row.component.css'],
   providers: [DatePipe],
@@ -14,7 +17,14 @@ import { DatePipe, CommonModule } from '@angular/common';
 export class EditRowComponent {
   @Input() bill: Bill = new Bill('', '', 0, new Date()); // Default initialization
   @Output() save: EventEmitter<Bill> = new EventEmitter<Bill>();
-  constructor(private datePipe: DatePipe) {}
+  
+  constructor(
+    private datePipe: DatePipe,
+    public dialogRef: MatDialogRef<EditRowComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.bill = data.bill;
+  }
 
   formatDate(date: Date): string {
     return this.datePipe.transform(date, 'yyyy-MM-dd') || ''; // Transform the date to string
@@ -31,11 +41,13 @@ export class EditRowComponent {
   onDueDateChange(value: string) {
     this.bill.dueDate = new Date(value); // Convert the input string to a Date object
   }
+
   onSave() {
-    this.save.emit(this.bill); //sends event up to parent
-    this.bill.isEditing = false;
+    this.save.emit(this.bill); // sends event up to parent
+    this.dialogRef.close();
   }
+
   onCancel() {
-    this.bill.isEditing = false;
+    this.dialogRef.close();
   }
 }
